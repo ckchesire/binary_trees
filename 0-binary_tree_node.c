db87ebb469872fdcad20561863492f8f2d1632f7
ckchesire <christianck.chesire@gmail.com>
2024-12-06 16:33:49 +0300

    Updated binary_trees.h to betty styling.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "binary_trees.h"
+
 /**
  * binary_tree_node - function that creates a binary tree node
  * @parent: pointer to the parent node of the node to create
--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,7 @@
 #ifndef B_TREES_H
 #define B_TREES_H
-
 /**
  * struct binary_tree_s - Binary tree node
- * 
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -11,18 +9,18 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
-// Binary Search Tree
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
-// AVL Tree
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
-// Max Binary Heap
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
