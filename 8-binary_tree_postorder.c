7bc70b0d78b87017063e8d5de5789c97817efb4c
ckchesire <christianck.chesire@gmail.com>
2024-12-07 04:27:13 +0300

    Implemented function for postorder traversal.

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "binary_trees.h"
-
 /**
  * binary_tree_postorder - function that goes through a binary tree using
  * post-order traversal
@@ -13,7 +12,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_postorder(tree->left, (*func)tree->n);
-	binary_tree_postorder(tree->right, (*func)tree->n);
-	binary_tree_postorder((*func)tree->n);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
